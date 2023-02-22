2effe3296b31a1c9b44ede0f7575f80569583e3e
sidsingh0 <sidsinghcs@gmail.com>
2023-02-23 03:12:10 +0530

    final cahanges neede

--- Aryan UI/index.js
+++ Aryan UI/index.js
@@ -1,23 +1,51 @@
-jQuery(document).ready(function($) {
-    // Scroll to the desired section on click
-    // Make sure to add the `data-scroll` attribute to your `<a>` tag.
-    // Example: 
-    // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
-    
-    function scrollToSection(event) {
-      event.preventDefault();
-      var $section = $($(this).attr('href')); 
-      $('html, body').animate({
-        scrollTop: $section.offset().top - $("#nav").height()
-      }, 500);
-    }
-    $('[data-scroll]').on('click', scrollToSection);
 
-    $('a').click(function(e){
-        e.preventDefault();
-        $('body, html').animate({
-            scrollTop: $('.scroll-to-this').offset().top - $("#nav").height()
-        }, 1000);
-    });
-    }
-  (jQuery));
+const observer= new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+      console.log(entry);
+      if (entry.isIntersecting){
+          entry.target.classList.add('show');
+      }
+      else{
+          //entry.target.classList.remove('show');
+      }
+  });
+});
+
+const observer2= new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+      console.log(entry);
+      if (entry.isIntersecting){
+        counter("count1", 0, 1000, 1500);
+        counter("count2", 0, 26, 2500);
+        counter("count3", 0, 12, 2500);
+      }
+      else{
+          //entry.target.classList.remove('show');
+      }
+  });
+});
+
+const observer3= new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+      console.log(entry);
+      if (entry.isIntersecting){
+        entry.target.classList.add('jello-horizontal');
+      }
+      else{
+          //entry.target.classList.remove('show');
+      }
+  });
+});
+
+
+const hiddenElements= document.querySelectorAll(".hidden")
+hiddenElements.forEach((el)=>observer.observe(el))
+
+const Counter= document.querySelectorAll(".counterclass");
+Counter.forEach((el)=>observer2.observe(el))
+
+const demo= document.querySelectorAll(".demo");
+demo.forEach((el)=>observer3.observe(el))
+
+
+
